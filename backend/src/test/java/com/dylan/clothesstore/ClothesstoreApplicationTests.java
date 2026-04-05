3008a5ada713ea28bdd7a179e859929505dde38b
Dylan Mc kenna <dylanmckenna@Dylans-MacBook-Air.local>
2026-04-05 14:43:15 +0100

    Initial project setup (backend + frontend)

---
+++ backend/src/test/java/com/dylan/clothesstore/ClothesstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dylan.clothesstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothesstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
